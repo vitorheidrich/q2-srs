934e5b5e685b4efa7fbeef46f8d74d4da80952d5
Vitor Heidrich <72625631+vitorheidrich@users.noreply.github.com>
2020-11-06 20:58:36 -0300

    Update plugin_setup.py

--- q2_srs/plugin_setup.py
+++ q2_srs/plugin_setup.py
@@ -44,5 +44,5 @@ plugin.methods.register_function(
                  'are lower than Cmin will be discarded.')
     },
     name='SRS',
-    description='Performs scaling with ranked subsampling normalization.'
+    description={'Performs scaling with ranked subsampling normalization.'}
 )
