82f6447556e524c0ec4442036766d56b83aa6b37
Vitor Heidrich <72625631+vitorheidrich@users.noreply.github.com>
2020-11-09 10:30:23 -0300

    Update _SRS.py

--- q2_srs/_SRS.py
+++ q2_srs/_SRS.py
@@ -1,6 +1,46 @@
+import os
+import tempfile
+import hashlib
+import subprocess
+import pkg_resources
+
 import biom
-import qiime2
+import skbio
+import qiime2.util
 import pandas as pd
+import q2templates
+
+def run_commands(cmds, verbose=True):
+    if verbose:
+        print("Running external command line application(s). This may print "
+              "messages to stdout and/or stderr.")
+        print("The command(s) being run are below. These commands cannot "
+              "be manually re-run as they will depend on temporary files that "
+              "no longer exist.")
+    for cmd in cmds:
+        if verbose:
+            print("\nCommand:", end=' ')
+            print(" ".join(cmd), end='\n\n')
+        subprocess.run(cmd, check=True)
 
 def SRS(table: biom.Table, c_min: int = 0) -> biom.Table:
-    return table
+    if table.is_empty():
+        raise ValueError("The provided table object is empty")
+        
+    if (c_min<=0):
+        raise ValueError("Please provide a valid c_min (> 0)")
+
+    norm_table = biom.Table()
+    
+    ## run the R script on the file
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+
+        ## write the biom table to file
+        input_name = os.path.join(temp_dir_name, 'table.tsv')
+        with open(input_name, 'w') as fh:
+            fh.write(table.to_tsv())
+
+
+        cmd = ['SRS.R', input_name, int(c_min), str(norm_table)]
+        run_commands([cmd])
+    return norm_table
