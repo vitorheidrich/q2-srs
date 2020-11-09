e5e3fff7d4466d9f59fefbe8715cf937c8eef910
Vitor Heidrich <72625631+vitorheidrich@users.noreply.github.com>
2020-11-09 14:52:51 -0300

    Update plugin_setup.py

--- q2_srs/plugin_setup.py
+++ q2_srs/plugin_setup.py
@@ -28,22 +28,22 @@ plugin = qiime2.plugin.Plugin(
 # Registering the SRS function
 plugin.methods.register_function(
     function=SRS,
-    inputs={'table': FeatureTable[Frequency],
-           'c_min': Int},
+    inputs={'table': FeatureTable[Frequency]},
     outputs=[('normalized_table', FeatureTable[Frequency])],
-    parameters={},
+    parameters={'c_min': Int},
     input_descriptions={
         'table': ('The feature table containing the '
-                 'samples to be normalized by SRS.'),
-        'c_min': ('The number of reads to which all samples will '
-                 'be normalized. Samples whose number of reads '
-                 'are lower than c_min will be discarded.')
+                 'samples to be normalized by SRS.')
     },
     output_descriptions={
         'normalized_table': ('SRS normalized feature table to '
                        'Cmin (integer) reads per sample.')
     },
-    parameter_descriptions={},
+    parameter_descriptions={
+        'c_min': ('The number of reads to which all samples will '
+                 'be normalized. Samples whose number of reads '
+                 'are lower than c_min will be discarded.')
+    },
     name='SRS normalization',
     description=('Performs scaling with ranked subsampling (SRS) normalization')
 )
