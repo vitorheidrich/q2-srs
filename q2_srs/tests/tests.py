babc4dcf6d3cab9ad8675a3f54b7bac1a1a82160
Vitor Heidrich <72625631+vitorheidrich@users.noreply.github.com>
2020-11-09 11:27:48 -0300

    Update tests.py

--- q2_srs/tests/tests.py
+++ q2_srs/tests/tests.py
@@ -23,4 +23,4 @@ def check_format(table: biom.Table):
     return()
             
           
-check_format(load_table("q2_srs/tests/feature-table.biom"))
+check_format(biom.load_table("q2_srs/tests/feature-table.biom"))
