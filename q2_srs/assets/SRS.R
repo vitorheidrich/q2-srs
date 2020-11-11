d28b70e1ce48701b2eb84bae0d8887393a0e97be
Vitor Heidrich <72625631+vitorheidrich@users.noreply.github.com>
2020-11-11 13:04:12 -0300

    Update SRS.R

--- q2_srs/assets/SRS.R
+++ q2_srs/assets/SRS.R
@@ -6,6 +6,11 @@ args <- commandArgs(TRUE)
 
 filename <- as.character(args[[1]])
 c_min <- as.numeric(args[[2]])
+set_seed <- as.character(args[[3]])
+seed <- as.numeric(args[[4]])
+
+if(set_seed%in%c("T","True")){set_seed<-T}
+else{set_seed<-F}
 #norm_data_name <- args[[3]]
 
 errQuit <- function(mesg, status=1) {
@@ -28,7 +33,7 @@ data <- read.table(file = filename, skip = 0, header = F,row.names = NULL,check.
 #include sample names
 colnames(data) <- colnames(read.csv(file = filename, nrows=1, skip=1, sep = "\t", check.names = FALSE))[-1]
 #normalize at c_min
-norm_data<-SRS(data,c_min)
+norm_data<-SRS(data, c_min, set_seed = set_seed, seed = seed)
 #include features names
 rownames(norm_data) <- read.table(file = filename, skip = 0, header = F, check.names = FALSE)[,1]
 
