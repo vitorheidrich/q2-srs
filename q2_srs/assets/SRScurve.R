9665d049d12167d987ac8d3b339d1f9df394b73d
Vitor Heidrich <72625631+vitorheidrich@users.noreply.github.com>
2020-11-11 13:05:51 -0300

    Update SRScurve.R

--- q2_srs/assets/SRScurve.R
+++ q2_srs/assets/SRScurve.R
@@ -1,4 +1,3 @@
-
 #! /usr/bin/Rscript
 #R  script to load SRS R package and run SRS function
 
@@ -6,20 +5,20 @@ cat(R.version$version.string, "\n")
 args <- commandArgs(TRUE)
 
 filename <- as.character(args[[1]])
-metric <- as.character(args[[2]])
+c_min <- as.numeric(args[[2]])
+set_seed <- as.character(args[[3]])
+seed <- as.numeric(args[[4]])
 
-#norm_data_name <- args[[3]]
+if(set_seed%in%c("T","True")){set_seed<-T}
+else{set_seed<-F}
 
-input_name, str(metric), str(step), str(sample),
-              str(max.sample.size), str(rarefy.comparison), str(rarefy.repeats),
-              str(rarefy.comparison.legend), str(SRS.color), str(label), str(output_dir)
 errQuit <- function(mesg, status=1) {
   message("Error: ", mesg)
   q(status=status)
 }
 
 #Check SRS installations and install SRS if needed
-if ("SRS" %in% installed.packages()[,"Package"] & packageVersion("SRS")=="0.1.2") { #UPDATE LATER
+if ("SRS" %in% installed.packages()[,"Package"] & packageVersion("SRS")=="0.2.0") { 
     cat("SRS package already installed! \n\n")
 } else {
     install.packages("SRS")
@@ -33,7 +32,7 @@ data <- read.table(file = filename, skip = 0, header = F,row.names = NULL,check.
 #include sample names
 colnames(data) <- colnames(read.csv(file = filename, nrows=1, skip=1, sep = "\t", check.names = FALSE))[-1]
 #normalize at c_min
-norm_data<-SRS(data,c_min)
+norm_data<-SRS(data, c_min, set_seed = set_seed, seed = seed)
 #include features names
 rownames(norm_data) <- read.table(file = filename, skip = 0, header = F, check.names = FALSE)[,1]
 
