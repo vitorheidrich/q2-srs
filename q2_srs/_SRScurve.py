f238e0596e546ca8a43c9cb1a50c06dbbbd75cac
Vitor Heidrich <72625631+vitorheidrich@users.noreply.github.com>
2020-11-17 11:15:05 -0300

    Update _SRScurve.py

--- q2_srs/_SRScurve.py
+++ q2_srs/_SRScurve.py
@@ -19,13 +19,13 @@ def run_commands(cmds, verbose=True):
     if verbose:
         print("Running external command line application(s). This may print "
               "messages to stdout and/or stderr.")
-        #print("The command(s) being run are below. These commands cannot "
-        #      "be manually re-run as they will depend on temporary files that "
-        #      "no longer exist.")
+        print("The command(s) being run are below. These commands cannot "
+              "be manually re-run as they will depend on temporary files that "
+              "no longer exist.")
     for cmd in cmds:
-        #if verbose:
-        #    print("\nCommand:", end=' ')
-        #    print(" ".join(cmd), end='\n\n')
+        if verbose:
+            print("\nCommand:", end=' ')
+            print(" ".join(cmd), end='\n\n')
         subprocess.run(cmd, check=True)
 
         
