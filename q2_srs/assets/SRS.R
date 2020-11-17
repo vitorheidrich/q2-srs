12f36c77e49dd978b0137a2fdc05db611e10223c
Vitor Heidrich <72625631+vitorheidrich@users.noreply.github.com>
2020-11-17 08:40:17 -0300

    Update SRS.R

--- q2_srs/assets/SRS.R
+++ q2_srs/assets/SRS.R
@@ -15,7 +15,7 @@ errQuit <- function(mesg, status=1) {
 }
 
 #Check SRS installations and install SRS if needed
-if ("SRS" %in% installed.packages()[,"Package"] & packageVersion("SRS")=="0.2.1") { 
+if ("SRS" %in% installed.packages()[,"Package"] & packageVersion("SRS")>="0.2.1") { 
     cat("SRS package already installed! \n\n")
 } else {
     install.packages("SRS")
